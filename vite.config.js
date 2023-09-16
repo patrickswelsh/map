e6ff097fd1cae9c0a08dd2f22727d1e8747db684
Patrick Welsh <patrickswelsh@gmail.com>
2023-09-16 12:47:41 -0400

    move dist folder

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 const root = resolve(__dirname,'src')
-const outDir = resolve(__dirname,'dist')
+const outDir = resolve(__dirname,'../dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
