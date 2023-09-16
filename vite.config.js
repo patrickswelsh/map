ec63cf53776e322118c5315c51616a291ef98785
Patrick Welsh <patrickswelsh@gmail.com>
2023-09-16 12:21:44 -0400

    added states page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+
+const root = resolve(__dirname,'src')
+const outDir = resolve(__dirname,'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
         ? 443
         : undefined,
   },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root,'index.html'),
+        states: resolve(root,'states','index.html')
+      }
+    }
+  }
 });
