5469150c0959991c874037a5a72fc16bcaf7483b
Patrick Welsh <patrickswelsh@gmail.com>
2023-12-02 11:28:42 -0500

    tidying

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ async function handlePlaceClick(event) {
     applyStyleToSelected(placelist);
 
     const place = await feature.fetchPlace();
-    const id = place.g.id;
+    const id = place.id;
     const zip = JSON.parse(place.displayName);
     const parsed = [id,zip]
 
