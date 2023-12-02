8b27388d1615780e2d82d0c0c6fac701b9c3f190
Patrick Welsh <patrickswelsh@gmail.com>
2023-12-02 10:48:15 -0500

    try again

--- src/index.ts
+++ src/index.ts
@@ -39,8 +39,7 @@ async function initMap() {
 }
 // Handle the click event.
 async function handlePlaceClick(event) {
-    event = JSON.parse(event)
-    let feature = event.features[0];
+    let feature = JSON.parse(event.features[0]);
     if (!feature.placeId) return;
     // Apply the style to the feature layer.
 
@@ -54,7 +53,6 @@ async function handlePlaceClick(event) {
     applyStyleToSelected(placelist);
 
     const place = await feature.fetchPlace();
-    
     const id = feature.placeId;
     const zip = JSON.parse(place.displayName);
     const parsed = [zip,id]
