1784e4479f7dd4cc08cd45e4891e6ee2d6cde114
Patrick Welsh <patrickswelsh@gmail.com>
2023-12-02 10:39:15 -0500

    hacky try

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ async function handlePlaceClick(event) {
     applyStyleToSelected(placelist);
 
     const place = await feature.fetchPlace();
-    const id = JSON.parse(feature.placeId.string);
+    const id = String(feature.placeId).substring(2,-2);
     const zip = JSON.parse(place.displayName);
     const parsed = [zip,id]
 
